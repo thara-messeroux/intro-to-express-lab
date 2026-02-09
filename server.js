1b7d60c1e2edfe782ec3bacd322619988f38c0cd
thara-messeroux <tharamesseroux@gmail.com>
2026-02-08 23:00:20 -0500

    Match GA shoes and finalize routes

--- server.js
+++ server.js
@@ -16,11 +16,15 @@ const collectibles = [
 
 // Data for shoes
 const shoes = [
-    { name: "Nike Air Max", price: 120, type: "sneaker" },
-    { name: "Adidas Ultraboost", price: 180, type: "sneaker" },
-    { name: "Clarks Desert Trek", price: 95, type: "casual" },
-    { name: "Allen Edmonds Park Avenue", price: 395, type: "dress" },
+    { name: "Birkenstocks", price: 50, type: "sandal" },
+    { name: "Air Jordans", price: 500, type: "sneaker" },
+    { name: "Air Mahomes", price: 501, type: "sneaker" },
+    { name: "Utility Boots", price: 20, type: "boot" },
+    { name: "Velcro Sandals", price: 15, type: "sandal" },
+    { name: "Jet Boots", price: 1000, type: "boot" },
+    { name: "Fifty-Inch Heels", price: 175, type: "heel" }
 ];
+
 
 
 // 1. Be Polite, Greet the User
